495b32a282b98f9fcb0ae0de8b71156bee02818e
Vijay Belwal <vbelwal73@gmail.com>
2024-02-17 16:44:27 +0530

    ZIPPY BASE SETUP

---
+++ Zippy/src/main/java/com/dogbytelab/Zippy/ZippyApplication.java
@@ -0,0 +1,13 @@
+package com.dogbytelab.Zippy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZippyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZippyApplication.class, args);
+	}
+
+}
---
+++ Zippy/src/test/java/com/dogbytelab/Zippy/ZippyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogbytelab.Zippy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZippyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
